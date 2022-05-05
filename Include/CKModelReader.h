9dd23620a58a212aa80e36462cc71388787fbe07
doyaGu <doyaGu@users.noreply.github.com>
2022-05-05 20:11:45 +0800

    Update CKModelReader.h

--- Include/CKModelReader.h
+++ Include/CKModelReader.h
@@ -41,20 +41,6 @@ See Also:Creation of Model Media Plugins
 	virtual CKERROR Load(CKContext* context,CKSTRING FileName,CKObjectArray *liste,CKDWORD LoadFlags,CKCharacter *carac=NULL) {	return CKERR_NOTIMPLEMENTED;	}
 
 /************************************************
-Summary: Loads a model file from memory.
-Return Value:
-	CK_OK if successful, an error code otherwise
-Arguments:
-	context: A pointer to the CKContext to use to create objects
-	iBuffer: pointer on the buffer to parse
-	iSize: size of the buffer to parse
-	liste: A pointer to a CKObjectArray that will be filled with the loaded objects.
-	LoadFlags: A combination of CK_LOAD_FLAGS
-See Also:Creation of Model Media Plugins
-************************************************/
-	virtual CKERROR Load(CKContext* context,const void* iBuffer, XDWORD iSize,CKObjectArray *liste,CKDWORD LoadFlags,CKCharacter *carac=NULL) {	return CKERR_NOTIMPLEMENTED;	}
-
-	/************************************************
 Summary: Saves a model file.
 Return Value:
 	CK_OK if successful, an error code otherwise
@@ -66,20 +52,6 @@ See Also:Creation of Model Media Plugins
 ************************************************/
 	virtual CKERROR Save(CKContext* context,CKSTRING FileName,CKObjectArray *liste,CKDWORD SaveFlags)	 {	return CKERR_NOTIMPLEMENTED;	}
 
-/************************************************
-Summary: Loads a model file from a stream.
-Return Value:
-	CK_OK if successful, an error code otherwise
-Arguments:
-	context: A pointer to the CKContext to use to create objects
-	inStream: reference to a VxStream object 
-	liste: A pointer to a CKObjectArray that will be filled with the loaded objects.
-	LoadFlags: A combination of CK_LOAD_FLAGS
-See Also:VxStream,Creation of Model Media Plugins
-************************************************/
-	// virtual CKERROR Load(CKContext* context,VxStream* stream,CKObjectArray *liste,CKDWORD LoadFlags,CKCharacter *carac=NULL) {	return CKERR_NOTIMPLEMENTED;	}
-	
-	
 };
 
 #endif
