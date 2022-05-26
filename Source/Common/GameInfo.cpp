3bcce35630323df9549b3d60657854558aa349b6
doyaGu <doyaGu@users.noreply.github.com>
2022-05-26 19:55:31 +0800

    Remove Common library

--- Source/Common/GameInfo.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameInfo.h"
-
-#include <string.h>
-
-CGameInfo::CGameInfo() : next(NULL), hkRoot(HKEY_CURRENT_USER)
-{
-    strcpy(regSubkey, "Software\\Terratools\\WorldMap\\Powerball");
-}
