e408c9d52f4d9835f73069c1fc81cded2cc5e56b
doyaGu <doyaGu@users.noreply.github.com>
2022-06-04 23:38:15 +0800

    Update GameInfo.h

--- Source/BuildingBlocks/TT_InterfaceManager_RT/GameInfo.h
+++ Source/BuildingBlocks/TT_InterfaceManager_RT/GameInfo.h
@@ -2,17 +2,52 @@
 #define BUILDINGBLOCKS_GAMEINFO_H
 
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
 #include <string.h>
+#include <windows.h>
 
 class CGameInfo
 {
 public:
-	CGameInfo() : next(NULL), hkRoot(HKEY_CURRENT_USER)
-	{
-		strcpy(regSubkey, "Software\\Terratools\\WorldMap\\Powerball");
-	}
-	
+    CGameInfo() : next(NULL),
+                  hkRoot(HKEY_CURRENT_USER),
+                  gameScore(0),
+                  levelScore(0),
+                  type(0),
+                  gameID(0),
+                  levelID(0),
+                  gameBonus(0),
+                  levelBonus(0),
+                  levelReached(0)
+    {
+        memset(gameName, 0, sizeof(gameName));
+        memset(levelName, 0, sizeof(levelName));
+        memset(fileName, 0, sizeof(fileName));
+        memset(path, 0, sizeof(path));
+        strcpy(regSubkey, "Software\\Terratools\\WorldMap\\Powerball");
+    }
+
+    CGameInfo(const char *path,
+              int type,
+              HKEY hkRoot,
+              const char *regSubKey,
+              const char *filename) : next(NULL),
+                                      gameScore(0),
+                                      levelScore(0),
+                                      gameID(0),
+                                      levelID(0),
+                                      gameBonus(0),
+                                      levelBonus(0),
+                                      levelReached(0)
+    {
+        memset(gameName, 0, sizeof(gameName));
+        memset(levelName, 0, sizeof(levelName));
+        strcpy(this->path, path);
+        this->type = type;
+        this->hkRoot = hkRoot;
+        strcpy(this->regSubkey, regSubKey);
+        strcpy(this->fileName, filename);
+    }
+
     virtual ~CGameInfo() {}
 
     CGameInfo *next;
