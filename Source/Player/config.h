242f3e65faaef1b4d1c66ce25e1f12dd4171589c
doyaGu <doyaGu@users.noreply.github.com>
2022-08-15 23:16:16 +0800

    Pump version

--- Source/Player/config.h
+++ Source/Player/config.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_PATCH 5
-#define VERSION "0.1.5"
+#define VERSION_PATCH 7
+#define VERSION "0.1.7"
 #define DESCRIPTION "Ballance Decompilation"
 
 #define DEFAULT_WIDTH 640
