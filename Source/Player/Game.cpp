09b177c9b7e4c5dca4282deda9a1e040184c779d
doyaGu <doyaGu@users.noreply.github.com>
2022-09-25 13:01:56 +0800

    Update EditScriptDefaultLevel

--- Source/Player/Game.cpp
+++ Source/Player/Game.cpp
@@ -8,7 +8,7 @@
 #include "Logger.h"
 #include "InterfaceManager.h"
 
-static bool EditScriptDefaultLevel()
+static void EditScriptDefaultLevel()
 {
     CNeMoContext *nc = CNeMoContext::GetInstance();
     assert(nc != NULL);
@@ -17,59 +17,85 @@ static bool EditScriptDefaultLevel()
 
     CKLevel *level = nc->GetCurrentLevel();
     if (!level)
-        return false;
+    {
+        CLogger::Get().Error("No level!");
+        return;
+    }
 
     CKBehavior *defaultLevel = nc->GetBehavior(level->ComputeObjectList(CKCID_BEHAVIOR), "Default Level");
     if (!defaultLevel)
-        return false;
+    {
+        CLogger::Get().Error("Unable to find script Default Level");
+        return;
+    }
+
+    bool success = false;
 
     // Set debug mode
+    if (config.debug)
     {
         CKBehavior *sd = nc->GetBehavior(defaultLevel, "set DebugMode");
-        if (!sd)
-            return false;
+        if (sd)
+        {
+            CKBehavior *bs = nc->GetBehavior(sd, "Binary Switch");
+            if (bs)
+            {
+                CKParameterLocal *local = sd->CreateLocalParameter("Condition", CKPGUID_BOOL);
+                CKBOOL debug = TRUE;
+                local->SetValue(&debug);
+                bs->GetInputParameter(0)->SetDirectSource(local);
 
-        CKBehavior *bs = nc->GetBehavior(sd, "Binary Switch");
-        if (!bs)
-            return false;
+                success = true;
+            }
+        }
 
-        CKParameterLocal *local = sd->CreateLocalParameter("Condition", CKPGUID_BOOL);
-        CKBOOL debug = config.debug ? TRUE : FALSE;
-        local->SetValue(&debug);
-        bs->GetInputParameter(0)->SetDirectSource(local);
+        if (!success)
+            CLogger::Get().Error("Failed to set debug mode");
+        success = false;
     }
 
     // Bypass "Set Language" script and set our language id
     {
         CKBehavior *sl = nc->GetBehavior(defaultLevel, "Set Language");
-        if (!sl)
-            return false;
+        if (sl)
+        {
+            CKBehavior *rr = nc->GetBehavior(sl, "TT_ReadRegistry");
+            if (rr)
+            {
+                CKBehaviorLink *linkOpRr = nc->RemoveBehaviorLink(sl, "Op", rr, 0, 0);
+                if (linkOpRr)
+                {
+                    CKBehavior *op1 = linkOpRr->GetInBehaviorIO()->GetOwner();
+                    nc->DestroyObject(linkOpRr);
 
-        CKBehavior *rr = nc->GetBehavior(sl, "TT_ReadRegistry");
-        if (!rr)
-            return false;
+                    CKBehaviorLink *linkRrOp = nc->RemoveBehaviorLink(sl, rr, "Op", 0, 0);
+                    if (linkRrOp)
+                    {
+                        CKBehavior *op2 = linkRrOp->GetOutBehaviorIO()->GetOwner();
+                        *(int *)op2->GetInputParameterReadDataPtr(0) = config.langId;
+                        nc->DestroyObject(linkRrOp);
 
-        CKBehaviorLink *linkOpRr = nc->RemoveBehaviorLink(sl, "Op", rr, 0, 0);
-        if (!linkOpRr)
-            return false;
-        CKBehavior *op1 = linkOpRr->GetInBehaviorIO()->GetOwner();
-        nc->DestroyObject(linkOpRr);
+                        nc->CreateBehaviorLink(sl, op1, op2, 0, 0);
 
-        CKBehaviorLink *linkRrOp = nc->RemoveBehaviorLink(sl, rr, "Op", 0, 0);
-        if (!linkRrOp)
-            return false;
-        CKBehavior *op2 = linkRrOp->GetOutBehaviorIO()->GetOwner();
-        *(int *)op2->GetInputParameterReadDataPtr(0) = config.langId;
-        nc->DestroyObject(linkRrOp);
+                        success = true;
+                    }
+                }
+            }
+        }
 
-        nc->CreateBehaviorLink(sl, op1, op2, 0, 0);
+        if (!success)
+            CLogger::Get().Error("Failed to set language id");
+        success = false;
     }
 
     int i;
 
     CKBehavior *sm = nc->GetBehavior(defaultLevel, "Screen Modes");
     if (!sm)
-        return false;
+    {
+        CLogger::Get().Error("Unable to find script Screen Modes");
+        return;
+    }
 
     CKBehavior *filters[3] = {NULL, NULL, NULL};
     for (i = 0; i < sm->GetSubBehaviorCount(); ++i)
@@ -94,31 +120,45 @@ static bool EditScriptDefaultLevel()
             }
         }
     }
-    for (i = 0; i < 3; ++i)
-        if (!filters[i])
-            return false;
 
     // Correct the bbp filter
-    int *bpp = (int *)filters[2]->GetInputParameterReadDataPtr(2);
-    *bpp = nc->GetBPP();
+    if (filters[2])
+    {
+        int *bpp = (int *)filters[2]->GetInputParameterReadDataPtr(2);
+        *bpp = nc->GetBPP();
+    }
+    else
+    {
+        CLogger::Get().Error("Failed to correct the bbp filter");
+    }
 
     // Unlock widescreen (Not 4:3)
     if (config.unlockWidescreen)
     {
         CKBehavior *ic = nc->GetBehavior(sm, "Insert Column", "ScreenModes");
-        if (!ic)
-            return false;
+        if (ic)
+        {
+            CKBehaviorLink *linkScIc = nc->RemoveBehaviorLink(sm, "Set Cell", ic, 0, 0);
+            if (linkScIc)
+            {
+                CKBehavior *sc = linkScIc->GetInBehaviorIO()->GetOwner();
+                sm->RemoveSubBehaviorLink(linkScIc);
+                nc->DestroyObject(linkScIc);
 
-        CKBehaviorLink *linkScIc = nc->RemoveBehaviorLink(sm, "Set Cell", ic, 0, 0);
-        if (!linkScIc)
-            return false;
-        CKBehavior *sc = linkScIc->GetInBehaviorIO()->GetOwner();
-        sm->RemoveSubBehaviorLink(linkScIc);
-        nc->DestroyObject(linkScIc);
+                if (filters[0])
+                {
+                    nc->RemoveBehaviorLink(sm, "Remove Column", filters[0], 0, 0, true);
 
-        nc->RemoveBehaviorLink(sm, "Remove Column", filters[0], 0, 0, true);
+                    nc->CreateBehaviorLink(sm, sc, filters[0], 0, 0);
 
-        nc->CreateBehaviorLink(sm, sc, filters[0], 0, 0);
+                    success = true;
+                }
+            }
+        }
+
+        if (!success)
+            CLogger::Get().Error("Failed to unlock widescreen");
+        success = false;
     }
 
     // Unlock high resolution
@@ -126,19 +166,32 @@ static bool EditScriptDefaultLevel()
     {
         const char *inBehName = (config.unlockWidescreen) ? "Set Cell" : "Remove Column";
         CKBehaviorLink *linkRri = nc->RemoveBehaviorLink(sm, inBehName, filters[0], 0, 0);
-        if (!linkRri)
-            return false;
-        CKBehavior *inBeh = linkRri->GetInBehaviorIO()->GetOwner();
-        nc->DestroyObject(linkRri);
+        if (linkRri)
+        {
+            CKBehavior *inBeh = linkRri->GetInBehaviorIO()->GetOwner();
+            nc->DestroyObject(linkRri);
 
-        nc->RemoveBehaviorLink(sm, filters[1], filters[2], 0, 0, true);
+            if (filters[1] && filters[2])
+            {
+                nc->RemoveBehaviorLink(sm, filters[1], filters[2], 0, 0, true);
 
-        nc->CreateBehaviorLink(sm, inBeh, filters[2], 0, 0);
+                nc->CreateBehaviorLink(sm, inBeh, filters[2], 0, 0);
+
+                success = true;
+            }
+        }
+
+        if (!success)
+            CLogger::Get().Error("Failed to unlock high resolution");
+        success = false;
     }
 
     CKBehavior *sts = nc->GetBehavior(defaultLevel, "Synch to Screen");
     if (!sts)
-        return false;
+    {
+        CLogger::Get().Error("Unable to find script Synch to Screen");
+        return;
+    }
 
     // Unlock frame rate limitation
     if (config.unlockFramerate)
@@ -152,22 +205,59 @@ static bool EditScriptDefaultLevel()
                 if (*frameRate == 3) // Frame Rate == Limit
                 {
                     *frameRate = 1; // Frame Rate = Free
+
+                    success = true;
                     break;
                 }
             }
         }
+
+        if (!success)
+            CLogger::Get().Error("Failed to unlock frame rate limitation");
+        success = false;
     }
 
     // Make it not to test 640x480 resolution
     {
-        CKBehavior *ii = nc->GetBehavior(sts, "Iterator If", "ScreenModes");
+        CKBehavior *ii = nc->GetBehavior(sts, "Iterator If");
         CKBehavior *delayer = nc->GetBehavior(sts, "Delayer");
-        if (!ii || !delayer)
-            return false;
+        if (ii && delayer)
+        {
+            CKBehaviorLink *linkTsIi1 = nc->GetBehaviorLink(sts, "Time Settings", ii, 0, 0);
+            CKBehaviorLink *linkTsIi2 = nc->GetBehaviorLink(sts, "Time Settings", ii, 0, 0, linkTsIi1);
+            if (linkTsIi1 && linkTsIi2)
+            {
+                CKBehavior *ts1 = linkTsIi1->GetInBehaviorIO()->GetOwner();
+                CKBehavior *ts2 = linkTsIi2->GetInBehaviorIO()->GetOwner();
 
-        nc->RemoveBehaviorLink(sts, ii, "TT Change ScreenMode", 1, 0, true);
-        nc->RemoveBehaviorLink(sts, "Show Mouse Cursor", delayer, 0, 0, true);
-        nc->CreateBehaviorLink(sts, ii, delayer, 1, 0);
+                sts->RemoveSubBehaviorLink(linkTsIi1);
+                sts->RemoveSubBehaviorLink(linkTsIi2);
+
+                nc->DestroyObject(linkTsIi1);
+                nc->DestroyObject(linkTsIi2);
+
+                CKBehaviorLink *linkDelayerCsm = nc->GetBehaviorLink(sts, delayer, "TT Change ScreenMode", 0, 0);
+                if (linkDelayerCsm)
+                {
+                    CKBehavior *csm2 = linkDelayerCsm->GetOutBehaviorIO()->GetOwner();
+                    CKBehaviorLink *linkCsmSmc = nc->RemoveBehaviorLink(sts, csm2, "Show Mouse Cursor", 0, 0);
+                    if (linkCsmSmc)
+                    {
+                        CKBehavior *smc2 = linkCsmSmc->GetOutBehaviorIO()->GetOwner();
+                        nc->DestroyObject(linkCsmSmc);
+
+                        nc->CreateBehaviorLink(sts, ts1, smc2, 0, 0);
+                        nc->CreateBehaviorLink(sts, ts2, smc2, 0, 0);
+
+                        success = true;
+                    }
+                }
+            }
+        }
+
+        if (!success)
+            CLogger::Get().Error("Failed to bypass 640x480 resolution test");
+        success = false;
     }
 
     // Skip Opening Animation
@@ -177,24 +267,31 @@ static bool EditScriptDefaultLevel()
         CKBehavior *ie = nc->GetBehavior(defaultLevel, "Intro Ende");
         CKBehavior *ml = nc->GetBehavior(defaultLevel, "Main Loading");
         CKBehavior *ps = nc->GetBehavior(defaultLevel, "Preload Sound");
-        if (!is || !ie || !ml || !ps)
-            return false;
+        if (is && ie && ml && ps)
+        {
+            CKBehaviorLink *linkStsIs = nc->GetBehaviorLink(defaultLevel, sts, is, 0, 0);
+            if (linkStsIs)
+            {
+                linkStsIs->SetOutBehaviorIO(ml->GetInput(0));
 
-        CKBehaviorLink *linkStsIs = nc->GetBehaviorLink(defaultLevel, sts, is, 0, 0);
-        if (!linkStsIs)
-            return false;
-        linkStsIs->SetOutBehaviorIO(ml->GetInput(0));
+                CKBehaviorLink *linkIeAs = nc->GetBehaviorLink(defaultLevel, ie, "Activate Script", 0, 0);
+                if (linkIeAs)
+                {
+                    CKBehaviorLink *linkPsWfa = nc->GetBehaviorLink(defaultLevel, ps, "Wait For All", 0, 0);
+                    if (linkPsWfa)
+                    {
+                        linkPsWfa->SetOutBehaviorIO(linkIeAs->GetOutBehaviorIO());
 
-        CKBehaviorLink *linkIeAs = nc->GetBehaviorLink(defaultLevel, ie, "Activate Script", 0, 0);
-        if (!linkIeAs)
-            return false;
-        CKBehaviorLink *linkPsWfa = nc->GetBehaviorLink(defaultLevel, ps, "Wait For All", 0, 0);
-        if (!linkPsWfa)
-            return false;
-        linkPsWfa->SetOutBehaviorIO(linkIeAs->GetOutBehaviorIO());
+                        success = true;
+                    }
+                }
+            }
+        }
+
+        if (!success)
+            CLogger::Get().Error("Failed to skip opening animation");
+        success = false;
     }
-
-    return true;
 }
 
 static int SetAsActiveCamera(const CKBehaviorContext &behcontext)
