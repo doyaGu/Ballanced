ab420aa0c4ba0800681bf2b0f3edef0f0cff7824
doyaGu <doyaGu@users.noreply.github.com>
2022-10-10 16:37:15 +0800

    TT_Gravity_RT: Add TT SpeedOMeter

--- Source/BuildingBlocks/TT_Gravity_RT/SpeedOMeter.cpp
+++ Source/BuildingBlocks/TT_Gravity_RT/SpeedOMeter.cpp
@@ -56,17 +56,80 @@ CKERROR CreateSpeedOMeterProto(CKBehaviorPrototype **pproto)
 int SpeedOMeter(const CKBehaviorContext &behcontext)
 {
     CKBehavior *beh = behcontext.Behavior;
-    CKContext *context = behcontext.Context;
+
+    CK3dEntity *target = (CK3dEntity *)beh->GetTarget();
+    if (!target)
+        return CKBR_OWNERERROR;
+
+    CKBOOL calcRelativeSpeed;
+    beh->GetLocalParameterValue(1, &calcRelativeSpeed);
+
+    int loopFrameDelay;
+    beh->GetLocalParameterValue(2, &loopFrameDelay);
+
+    float delta = behcontext.DeltaTime;
+    if (loopFrameDelay > 1)
+        delta *= loopFrameDelay;
+
+    VxVector lastPos;
+    beh->GetLocalParameterValue(0, &lastPos);
+
+    VxVector pos;
+    target->GetPosition(&pos);
+
+    VxVector difference = pos - lastPos;
+    float speed = Magnitude(difference) * 1000.0f / delta;
+    beh->SetOutputParameterValue(0, &speed);
+
+    if (calcRelativeSpeed)
+    {
+        float minSpeed = 0.0f;
+        beh->GetInputParameterValue(0, &minSpeed);
+        float maxSpeed = 0.0f;
+        beh->GetInputParameterValue(1, &maxSpeed);
+
+        speed = (speed - minSpeed) / (maxSpeed - minSpeed);
+        if (speed > 1.0f)
+            speed = 1.0f;
+        else if (speed < 0.0f)
+            speed = 0.0f;
+
+        beh->SetOutputParameterValue(1, &speed);
+    }
+
+    beh->SetLocalParameterValue(0, &pos);
+    beh->ActivateInput(0, FALSE);
+    beh->ActivateOutput(0, TRUE);
+    return CKBR_OK;
 }
 
 CKERROR SpeedOMeterCallBack(const CKBehaviorContext &behcontext)
 {
     CKBehavior *beh = behcontext.Behavior;
 
-    switch (behcontext.CallbackMessage)
+    if (behcontext.CallbackMessage == CKM_BEHAVIORSETTINGSEDITED)
     {
-    default:
-        break;
+        CKBOOL calcRelativeSpeed;
+        beh->GetLocalParameterValue(1, &calcRelativeSpeed);
+        int inputParamCount = beh->GetInputParameterCount();
+        if (calcRelativeSpeed)
+        {
+            if (inputParamCount == 0)
+            {
+                beh->CreateInputParameter("Minimum Speed", CKPGUID_FLOAT);
+                beh->CreateInputParameter("Maximum Speed", CKPGUID_FLOAT);
+                beh->CreateOutputParameter("Relative Speed", CKPGUID_FLOAT);
+            }
+        }
+        else if (inputParamCount != 0)
+        {
+            CKParameterIn *pin = beh->RemoveInputParameter(0);
+            CKDestroyObject(pin);
+            pin = beh->RemoveInputParameter(0);
+            CKDestroyObject(pin);
+            CKParameterOut *pout = beh->RemoveOutputParameter(0);
+            CKDestroyObject(pout);
+        }
     }
 
     return CKBR_OK;
