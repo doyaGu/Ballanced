e4133bb428d0fdd3f9e8fffda260828968d937bb
doyaGu <10153309+doyaGu@users.noreply.github.com>
2024-03-04 15:49:25 -0500

    physics_RT: Fix CKIpionManager::UpdateObjectWorldMatrix()

--- Source/BuildingBlocks/physics_RT/CKIpionManager.cpp
+++ Source/BuildingBlocks/physics_RT/CKIpionManager.cpp
@@ -783,8 +783,8 @@ void CKIpionManager::UpdateObjectWorldMatrix(IVP_Real_Object *obj)
     VxMatrix m;
 
     // Transpose
-    for (int i = 3; i >= 0; --i)
-        for (int j = 3; j >= 0; --j)
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
             m[j][i] = (float)mat.get_elem(i, j);
     m[0][3] = 0.0f;
     m[1][3] = 0.0f;
