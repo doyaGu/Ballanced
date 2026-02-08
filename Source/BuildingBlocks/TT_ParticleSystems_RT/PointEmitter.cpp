c2c79a04dfa99d50f7c641aa4c569d886a46bbbd
doyaGu <doyaGu@users.noreply.github.com>
2026-02-08 15:49:08 -0500

    Remove source files

--- Source/BuildingBlocks/TT_ParticleSystems_RT/PointEmitter.cpp
+++
@@ -1,9 +0,0 @@
-#include "CKAll.h"
-
-#include "PointEmitter.h"
-
-void PointEmitter::InitiateParticle(Particle *p)
-{
-    CK3dEntity *entity = (CK3dEntity *)m_Context->GetObject(m_Entity);
-    entity->GetPosition(&(p->pos));
-}
